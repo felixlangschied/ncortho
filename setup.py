d5667127dd5053a87133b56a590a55fcdff602a1
felixlangschied <felix_langschied@web.de>
2021-09-16 16:46:33 +0200

    minor

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ncOrtho",
-    version="0.2.1",
+    version="0.2.2",
     python_requires='>=3.7.0',
     description=" Targeted ortholog search for miRNAs ",
     long_description=long_description,
